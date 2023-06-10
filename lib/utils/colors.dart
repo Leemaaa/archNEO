00666c21fb76e80263999ea9e36d8fdd674bfbf8
Clushka <97032059+Clushka@users.noreply.github.com>
2023-06-10 23:06:39 +0600

    Delete lib directory

--- lib/utils/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const red = Colors.red;
-const white = Colors.white;
-const black = Colors.black;
-const grey = Colors.grey;
-const brown = Colors.brown;
-const yellow = Color.fromRGBO(245, 186, 65, 1);
